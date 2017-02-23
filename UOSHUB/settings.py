7ca3dc4ef120589519b24b8f3a98b2c08880cdae
Omar Einea <eineao@gmail.com>
2017-02-23 10:57:20 +0300

    Install All Sub-Apps to UOSHUB

--- UOSHUB/settings.py
+++ UOSHUB/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Blackboard',
+    'myUDC',
+    'Office',
+    'UOS',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
