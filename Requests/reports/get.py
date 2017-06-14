17edda2f47870cf0bbd519e20bab588ec4b3ddae
Omar Einea <eineao@gmail.com>
2017-06-14 13:41:03 +0300

    Documentation For Reports Package

--- Requests/reports/get.py
+++ Requests/reports/get.py
@@ -1,83 +1,126 @@
 import requests
 
 
+# General report request with common attributes
 def request(options):
+    # HTTP post request
     return requests.post(
+        # Root UOS reports url
         url="https://uos.sharjah.ac.ae:9050/reports/rwservlet",
+        # Parameters used across all report requests
         data=dict({
+            # Request from UOS server
             "server": "RptSvr_uosas5_INB_asinst",
+            # Get report in XML format
             "desformat": "xml",
+            # Cache the report until it's processed
             "destype": "cache",
+            # User id of reports server
             "userid": "uos_mis/u_pick_it@PROD"
+            # Append report specific extra parameters
         }, **options)
     )
 
 
+# Gets student's personal information
 def personal_info(sid):
-    response = request({
+    return request({
+        # Report cipher
         "REPORT": "SYREXDT_REP",
+        # Student id
         "P_SPRIDEN_ID": sid.upper()
-    })
-    response.encoding = "utf-8"
-    return response.text.encode()
+        # Encode content in utf-8 as it contains Arabic
+    }).content.decode()
 
 
+# Gets student's summarized schedule
 def schedule(sid, semester):
     return request({
+        # Report cipher
         "REPORT": "SYFSSCE_REP",
+        # Student ids range (from, to)
         "P_ID_FROM": sid.upper(),
         "P_ID_TO": sid.upper(),
+        # Semester code
         "P_TERM_CODE": semester
     }).text
 
 
+# Gets student's final exams schedule
 def final_exams(sid, semester):
     return request({
+        # Report cipher
         "REPORT": "SYRSSFE_REP",
+        # Student id
         "P_ID": sid.upper(),
+        # Semester code
         "P_TERM_CODE": semester
     }).text
 
 
+# Gets student's whole study plan
 def study_plan(sid, reg_semester):
     return request({
+        # Report cipher
         "REPORT": "SYRSPOS_REP",
+        # Still don't know these two
         "P_PROG_CODE": "ALL",
         "P_EXP_GRD": "ALL",
+        # Collage number
         "P_COLL_CODE": "ALL",
+        # Campus abbreviation
         "P_CAMP_CODE": "ALL",
+        # Degree level initials
         "P_LEVEL_CODE": "ALL",
+        # Student id
         "P_STUDENT_ID": sid.upper(),
-        # Student enrollment semester
+        # Student enrollment semester code
         "P_TERM_CODE": reg_semester
     }).text
 
 
+# Gets offered courses catalog for a semester
 def offered_courses(semester):
     return request({
+        # Report cipher
         "REPORT": "SYRSCHE_REP",
+        # Campus abbreviation
         "CAMP": "%",
+        # Collage number
         "COLL": "%",
+        # Department short name
         "DEPT": "%",
+        # Degree level initials
         "LEVL": "ALL",
+        # Major registration restrictions
         "P_IND": "ALL",
-        "P_WEB": "ALL",
+        # Availability for web add/drop
+        "P_WEB": "Y",
+        # Class capacity range (min, max)
         "MAX": "258",
         "MIN": "0",
+        # Semester code
         "TERM": semester
     }).text
 
 
+# Gets student's unofficial transcript
 def unofficial_transcript(sid):
     return request({
+        # Report cipher
         "REPORT": "SYFTRTE_REP",
+        # Campus abbreviation
         "P_CAMP": "ALL",
+        # Degree level initials
         "P_LEVL_CODE": "ALL",
+        # Collage number
         "P_COLL_CODE": "ALL",
+        # Still don't know these three
         "P_NATION_CODE": "ALL",
         "P_SPONSOR_ID": "ALL",
         "P_EXP_GRD": "ALL",
         # Whether to show in progress courses
         "P_INPROG_CRS_IND": "Y",
+        # Student id
         "P_ID": sid.upper()
     }).text
--- Requests/reports/scrape.py
+++ Requests/reports/scrape.py
@@ -1,12 +1,20 @@
 from lxml.etree import fromstring as __parse_xml
 
 
+# Scrapes new grades from transcript that aren't in known course
 def new_grades(transcript, semester, known_courses):
+    # Dictionary to store new grades
     grades = {}
+    # Loop through the available semesters in transcript
     for term in __parse_xml(transcript).find(".//LIST_G_ACADEMIC_HIST_TERM"):
+        # If the semester is the selected one
         if term.find("TERM_TERM_DESC").text == semester:
+            # Loop through courses in that semester
             for course in term.find("LIST_G_ACADEMIC_HIST_DETAILS"):
+                # Store course title
                 course_title = course.find("COURSE_TITLE").text
+                # If course isn't already among the known courses
                 if course_title not in known_courses:
+                    # Add it's grade to the new grades dictionary
                     grades[course_title] = course.find("GRDE_CODE_FINAL").text
     return grades
