cf60f033f45f50efc87f2c6e92a8a1164cb73c3c
Omar Einea <eineao@gmail.com>
2017-06-15 20:31:33 +0300

    Documentation & Tweaks For Blackboard Package

--- Requests/blackboard/get.py
+++ Requests/blackboard/get.py
@@ -1,75 +1,104 @@
 import requests
 
+# Root url of Blackboard
 root_url = "https://elearning.sharjah.ac.ae/webapps/"
-
-__QUERIES = {
-    "Announcements": 1,
-    "Courses": 4,
-    "Organizations": 5,
-    "Tasks": 7,
-    "Notes": 12,
-    "Grades": 20,
-    "Collages": 28,
-    "Surveys": 257
-}
+# General Blackboard items id format
+__id = "_{}_1"
 
 
+# Logs in Blackboard and returns the session
 def __login(sid, pin):
     return requests.post(
+        # Post data to login url
         root_url + "login/",
+        # Send student id and password
         data={"user_id": sid, "password": pin}
     ).cookies
 
 
+# Gets list of one of the options listed below through AJAX
 def list_of(session, query):
-    return requests.post(
+    return requests.get(
+        # Get data from AJAX requests url
         root_url + "portal/execute/tabs/tabAction",
+        # Send login session
         cookies=session,
-        data={
+        # Send required data
+        params={
+            # Get list through AJAX
             "action": "refreshAjaxModule",
-            "modId": "_{}_1".format(__QUERIES[query]),
-            "tabId": "_1_1",
+            # Get list of one of these options
+            "modId": __id.format({
+                # Available lists
+                "Announcements": 1,
+                "Courses": 4,
+                "Organizations": 5,
+                "Tasks": 7,
+                "Notes": 12,
+                "Grades": 20,
+                "Collages": 28,
+                "Surveys": 257
+                # Select one of them
+            }[query]),
+            # Required parameter
+            "tabId": __id.format(1),
         }
-    ).text
+    ).url
 
 
+# Gets course's specific menu (on the left side)
 def course_menu(session, course_id):
     return requests.get(
+        # Get data from course menu url
         root_url + "blackboard/content/courseMenu.jsp",
+        # Send login session
         cookies=session,
-        params={"course_id": course_id}
+        # Specify requested course id
+        params={"course_id": __id.format(course_id)}
     ).text
 
 
+# Gets page of announcements for all courses or for one course by id
 def announcements(session, course_id=None):
     return requests.get(
+        # Get data from announcements url
         root_url + "blackboard/execute/announcement",
+        # Send login session
         cookies=session,
-        params={
-            "method": "search",
-            "searchSelect": course_id or "announcement.coursesonly.label"
-        }
-    ).text
+        # By default get all course announcements, if course id is sent then only get the sent one
+        params={"method": "search", "searchSelect": course_id or "announcement.coursesonly.label"}
+    ).url
 
 
+# Gets student's Blackboard profile image page
 def profile_image(session):
     return requests.get(
+        # Get data from Blackboard flyout menu url
         root_url + "portal/execute/globalNavFlyout",
+        # Send login session
         cookies=session,
+        # Required parameter
         params={"cmd": "view"}
     ).text
 
 
+# Get direct list of courses
 def courses(session):
     return requests.get(
+        # Get data from top right navigation menu
         root_url + "blackboard/execute/globalCourseNavMenuSection",
+        # Send login session
         cookies=session,
+        # Required parameter
         params={"cmd": "view"}
     ).text
 
 
+# Gets the (not used) tasks page
 def __tasks(session):
     return requests.get(
+        # Get data from tasks management page
         root_url + "blackboard/execute/taskEditList",
+        # Send login session
         cookies=session
     ).text
--- Requests/blackboard/scrape.py
+++ Requests/blackboard/scrape.py
@@ -1,29 +1,44 @@
+# Import two html processing functions as hidden variables
 from lxml.html import fromstring as __parse_html, tostring as __get_html
 
 
+# Scrapes courses ids from list_of("Courses") and courses()
 def courses(response, from_list_of=False):
-    return {
-        link.text[:7]: from_list_of and link.attrib["href"][54:-7] or link.attrib["onclick"][87:-24]
-        for link in __parse_html(response).xpath("//a")
-    }
+    # Determine scraped attribute and location of course id
+    # depending on whether the sent data is from list_of() or courses() functions
+    attr, start, end = ("href", 54, -7) if from_list_of else ("onclick", 87, -24)
+    # Return dictionary of courses blackboard ids mapped to courses myUDC ids
+    return {link.text[:7]: link.attrib[attr][start:end] for link in __parse_html(response).xpath("//a")}
 
 
+# Scrapes announcements' useful data
 def announcements(response):
+    # Array to store announcements dictionaries
     messages = []
+    # Loop through announcements
     for item in __parse_html(response).xpath("//ul[@id='announcementList']/li"):
+        # Add announcement to the array
         messages.append({
+            # Clear announcement title from white spaces and store it
             "title": item.xpath("h3[@class='item']")[0].text.strip(),
+            # Store raw announcement body in html format after encoding in utf-8
             "body": __get_html(item.xpath(".//div[@class='vtbegenerated']")[0]).decode(),
+            # Store tag's all children text content as date using text_content()
             "date": item.xpath("div[@class='details']/p")[0].text_content(),
+            # Store announcement's associated course id
             "id": item.xpath(".//span[@class='courseId']")[0].text[:7],
         })
     return messages
 
 
+# Scraps url of Blackboard profile image
 def profile_image(response):
     return __parse_html(response).xpath("//a[@id='profileLink']/img/@src")[0]
 
 
+# Scrapes list of courses from survey panel (obsolete)
 def __survey_courses(response):
+    # Using Regex and Json
     import re, json
+    # Search for the Json object that contains the course list and parse it
     return json.loads(re.search("json_ecb = ({.*?});", response).group(1))
