c3ba38c5f8ee4b017733ab623791585359379660
Omar Einea <eineao@gmail.com>
2017-06-20 19:11:24 +0300

    Add A General Blackboard Request Function

--- Requests/blackboard/get.py
+++ Requests/blackboard/get.py
@@ -16,89 +16,79 @@ def __login(sid, pin):
     ).cookies
 
 
-# Gets list of one of the options listed below through AJAX
-def list_of(session, query):
+# General Blackboard data request with common attributes
+def data(link, session, params=None):
     return requests.get(
-        # Get data from AJAX requests url
-        root_url + "portal/execute/tabs/tabAction",
+        # Get data from root url + sub url
+        root_url + link,
         # Send login session
         cookies=session,
         # Send required data
-        params={
-            # Get list through AJAX
-            "action": "refreshAjaxModule",
-            # Get list of one of these options
-            "modId": __id.format({
-                # Available lists
-                "Announcements": 1,
-                "Courses": 4,
-                "Organizations": 5,
-                "Tasks": 7,
-                "Notes": 12,
-                "Grades": 20,
-                "Collages": 28,
-                "Surveys": 257
-                # Select one of them
-            }[query]),
-            # Required parameter
-            "tabId": __id.format(1),
-        }
-    ).url
+        params=params
+    ).text
+
+
+# Gets list of one of the options listed below through AJAX
+def list_of(session, query):
+    # Get data from AJAX requests url
+    return data("portal/execute/tabs/tabAction", session, {
+        # Get list through AJAX
+        "action": "refreshAjaxModule",
+        # Get list of one of these options
+        "modId": __id.format({
+            # Available lists
+            "Announcements": 1,
+            "Courses": 4,
+            "Organizations": 5,
+            "Tasks": 7,
+            "Notes": 12,
+            "Grades": 20,
+            "Collages": 28,
+            "Surveys": 257
+            # Select one of them
+        }[query]),
+        # Required parameter
+        "tabId": __id.format(1),
+    })
 
 
 # Gets course's specific menu (on the left side)
 def course_menu(session, course_id):
-    return requests.get(
-        # Get data from course menu url
-        root_url + "blackboard/content/courseMenu.jsp",
-        # Send login session
-        cookies=session,
+    # Get data from course menu url
+    return data("blackboard/content/courseMenu.jsp", session, {
         # Specify requested course id
-        params={"course_id": __id.format(course_id)}
-    ).text
+        "course_id": __id.format(course_id)
+    })
 
 
 # Gets page of announcements for all courses or for one course by id
 def announcements(session, course_id=None):
-    return requests.get(
-        # Get data from announcements url
-        root_url + "blackboard/execute/announcement",
-        # Send login session
-        cookies=session,
+    # Get data from announcements url
+    return data("blackboard/execute/announcement", session, {
         # By default get all course announcements, if course id is sent then only get the sent one
-        params={"method": "search", "searchSelect": course_id or "announcement.coursesonly.label"}
-    ).url
+        "method": "search", "searchSelect": course_id or "announcement.coursesonly.label"
+    })
 
 
 # Gets student's Blackboard profile image page
 def profile_image(session):
-    return requests.get(
-        # Get data from Blackboard flyout menu url
-        root_url + "portal/execute/globalNavFlyout",
-        # Send login session
-        cookies=session,
+    # Get data from Blackboard flyout menu url
+    return data("portal/execute/globalNavFlyout", session, {
         # Required parameter
-        params={"cmd": "view"}
-    ).text
+        "cmd": "view"
+    })
 
 
 # Get direct list of courses
 def courses(session):
-    return requests.get(
-        # Get data from top right navigation menu
-        root_url + "blackboard/execute/globalCourseNavMenuSection",
-        # Send login session
-        cookies=session,
+    # Get data from top right navigation menu
+    return data("blackboard/execute/globalCourseNavMenuSection", session, {
         # Required parameter
-        params={"cmd": "view"}
-    ).text
+        "cmd": "view"
+    }).text
 
 
 # Gets the (not used) tasks page
 def __tasks(session):
-    return requests.get(
-        # Get data from tasks management page
-        root_url + "blackboard/execute/taskEditList",
-        # Send login session
-        cookies=session
-    ).text
+    # Get data from tasks management page
+    return data("blackboard/execute/taskEditList", session)
