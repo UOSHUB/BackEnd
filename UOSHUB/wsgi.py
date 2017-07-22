ded5fa3e75a296e19384797159183fb38a827236
Omar Einea <eineao@gmail.com>
2017-07-22 20:49:51 +0300

    Remove Unused Default Features & Clean Up Settings

--- UOSHUB/wsgi.py
+++ UOSHUB/wsgi.py
@@ -1,11 +1,4 @@
-"""
-WSGI config for UOSHUB project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
+""" WSGI config for UOSHUB project. """
 
 import os
 
