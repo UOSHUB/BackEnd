ab0de6821b76d119295d7f4f6f616d1f24fd3ce1
OmarEinea <eineao@gmail.com>
2017-08-06 17:48:26 +0300

    Add General Blackboard API & Student Info Getter Functions

--- Requests/blackboard/get.py
+++ Requests/blackboard/get.py
@@ -24,6 +24,30 @@ def __login(sid, pin):
     return response.cookies.get_dict()
 
 
+# General Blackboard API request with common attributes
+def api(link, session, params=None):
+    return requests.get(
+        # Get data from root url + api url
+        root_url + 'learn/api/public/v1/' + link,
+        # Send login session
+        cookies=session,
+        # Send required data
+        params=params
+    ).json()
+
+
+# Get student's basic info (name, major, collage)
+def basic_info(session, sid):
+    # Request data from API url while passing student id
+    student = api('users/userName:' + sid, session, {'fields': 'name,job'})
+    # Extract and return a dictionary of student info
+    return {
+        'name': ' '.join(student['name'].values()),
+        'major': student['job']['department'],
+        'collage': student['job']['company']
+    }
+
+
 # General Blackboard request to 'webapps/' with common attributes
 def web(link, session, params=None):
     return requests.get(
