ac38c199a1b6aacf675033a5da334dbef05730a2
OmarEinea <eineao@gmail.com>
2017-08-08 23:20:11 +0300

    Preserve DB During Deployment & Clean Up Unused Static Files
    Also use python3 command instead of its alias inside the script
    and make the deploy script executable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
+
 # Clean up cached static files
 rm -r static Website/static/min
+
+# Backup database file
+mv db.sqlite3 db.save
 
 # Sync back-end repo with GitHub
 git fetch --all
@@ -11,9 +16,15 @@ git fetch --all
 git reset --hard origin/master
 cd ../..
 
+# Restore database file
+mv db.save db.sqlite3
+
 # Compress and collect static files
-python manage.py compress
-python manage.py collectstatic
+python3 manage.py compress
+python3 manage.py collectstatic
+
+# Clean up uncompressed static files
+rm -r static/css static/js
 
 # Restart Gunicorn and Nginx
 systemctl restart gunicorn
