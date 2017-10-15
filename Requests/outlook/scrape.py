e58d0b6dbc2c1ca10c45839d388fcc0109d8900b
Omar Einea <eineao@gmail.com>
2017-10-15 11:57:09 +0400

    Add A Function To Scrape Student's Personal Emails Details

--- Requests/outlook/scrape.py
+++ Requests/outlook/scrape.py
@@ -1,6 +1,9 @@
 from Requests import clean_course_name as __clean
 import re
 
+# Dictionary & Regex to extract "Reply" & "Forward" emails
+__events = {"re": "Reply", "fw": "Forward"}
+__split_subject = re.compile("^(?:(re|fw|fwd): )?(.*)$", re.IGNORECASE)
 # Regex expression to extract required details from generated emails
 __content = re.compile("(.+): (?P<title>.+) has been added to course: (?P<course>.+)\. Click")
 __assignment = re.compile("Assignment: (?P<title>.+) in course: (?P<course>.+) is due: .+, (.{3}).* ([0-9]+),")
@@ -35,6 +38,30 @@ def emails_previews(emails):
                 "sender": email["Sender"]["EmailAddress"]["Name"]
             })
     return previews
+
+
+# Scrapes personal emails details
+def personal_emails(raw_emails):
+    # Array to store emails
+    emails = []
+    # Loop through raw emails
+    for email in raw_emails:
+        # Match and store email event (if any), title and sender
+        event, title = __split_subject.match(email["Subject"]).groups()
+        sender = email["Sender"]["EmailAddress"]
+        # Add extracted email details to emails
+        emails.append({
+            # Extract event and title using Regex matches
+            "title": title,
+            "event": __events[event[:2].lower()] if event else "New Email",
+            # Get sender name and email
+            "sender": sender["Name"],
+            "from": sender["Address"],
+            # Get email time and body directly
+            "time": email["DateTimeSent"],
+            "body": email["Body"]["Content"],
+        })
+    return emails
 
 
 # Scrapes Blackboard generated courses emails
