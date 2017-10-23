804152da95676e86530329b0d268892d989f3641
Omar Einea <eineao@gmail.com>
2017-10-23 10:43:00 +0400

    Add The Main Threaded Course Grades Fetching Logic In API

--- Requests/views/grades.py
+++ Requests/views/grades.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .common import login_required
 from Requests import blackboard
+from threading import Thread
 
 
 # Grades requests handler
@@ -13,6 +14,37 @@ class Grades(APIView):
     # Returns student's array of grades on GET request
     @login_required("blackboard")
     def get(self, request, term):
+        # If requesting grades of a term
+        if term:
+            # Initialize empty objects & store Blackboard cookies
+            grades, threads = {}, []
+            cookies = request.session["blackboard"]
+
+            # A single course's grades fetching function for threading
+            def course_grades(course_key, blackboard_id):
+                # Get & scrape then add course's grades to dictionary
+                grades[course_key] = {
+                    "grades": blackboard.scrape.course_grades(
+                        blackboard.get.course_grades(
+                            # Send Blackboard cookies & course blackboard id
+                            cookies, blackboard_id
+                        )
+                    )
+                }
+            # Get & scrape then loop through Blackboard courses in term
+            for key, course in blackboard.scrape.courses_by_term(
+                # Send Blackboard cookies to "get" and term id to "scrape"
+                blackboard.get.courses_list(cookies), term
+            ).items():
+                # Construct a thread to get each course's grades in parallel
+                thread = Thread(target=course_grades, args=(key, course["bb"]))
+                # Start the thread and add it to threads
+                thread.start()
+                threads.append(thread)
+            # Loop through all threads and join them to main thread
+            [thread.join() for thread in threads]
+            # Return all courses' grades after all threads are done
+            return Response(grades)
         # If requesting grades API root path
         return Response({
             # Return a dictionary of {term name: url of grades in term} pairs
