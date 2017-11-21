46fa551fb277a323b51f4b545cf64281ae4c9934
Omar Einea <eineao@gmail.com>
2017-11-21 23:14:16 +0400

    Add A Function To Submit An Assignment Files To Blackboard

--- Requests/blackboard/edit.py
+++ Requests/blackboard/edit.py
@@ -17,3 +17,35 @@ def dismiss_update(session, update):
         "c0-param0": "string:" + str(update),
         "batchId": "0"
     })
+
+
+def submit_files(session, course_id, content_id, files):
+    course_id, content_id = __id(course_id), __id(content_id)
+    nonce, nonce_ajax = __get_nonce.search(
+        requests.get(
+            __new_submission_url.format(course_id, content_id),
+            cookies=session
+        ).text
+    ).groups()
+    data = [
+        ("blackboard.platform.security.NonceUtil.nonce", nonce),
+        ("blackboard.platform.security.NonceUtil.nonce.ajax", nonce_ajax),
+        ("course_id", course_id),
+        ("content_id", content_id),
+        ("dispatch", "submit")
+    ]
+    files = [(
+        ("newFile_LocalFile" + str(index), file),
+        data.extend([
+            ("newFile_linkTitle", file[0]),
+            ("newFile_attachmentType", "L")
+        ])
+    )[0] for index, file in enumerate(
+        files if isinstance(files, list) else [files]
+    )]
+    return requests.post(
+        __submit_files_url,
+        cookies=session,
+        files=files,
+        data=data
+    )
