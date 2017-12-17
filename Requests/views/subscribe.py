54c9c67aeb32102a99792b7b6733379ccceaa149
Omar Einea <eineao@gmail.com>
2017-12-17 18:22:56 +0400

    Add An API View For Subscribing & Unsubscribing To Services

---
+++ Requests/views/subscribe.py
@@ -0,0 +1,37 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from Requests.models import Student
+from .common import login_required
+
+
+# Subscribe requests handler
+class Subscribe(APIView):
+    # Subscribe to services on POST request
+    @staticmethod
+    @login_required()
+    def post(request):
+        # Store current student id
+        sid = request.session["sid"]
+        # If student isn't stored among the subscribers
+        if not Student.objects.filter(sid=sid).exists():
+            # Save student in the database
+            Student(sid=sid).save()
+            # Return CREATED response
+            return Response(status=201)
+        # Otherwise return ALREADY REPORTED response
+        return Response(status=208)
+
+    # Unsubscribe from services on DELETE request
+    @staticmethod
+    @login_required()
+    def delete(request):
+        # Store current student database model object
+        student = Student.objects.filter(sid=request.session["sid"])[0]
+        # If student is stored among the subscribers
+        if student.exists():
+            # Remove student from the database
+            student.delete()
+            # Return NO CONTENT response
+            return Response(status=204)
+        # Otherwise return GONE response
+        return Response(status=410)
