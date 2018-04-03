5f378ee9ef2a80ea3fafea8a85a68045c79a7af0
Omar Einea <eineao@gmail.com>
2018-04-03 22:56:43 +0300

    Add A Feature Of Accessing UOS Websites Through Short Abbreviations

--- Website/urls.py
+++ Website/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # Captures special abbreviations and redirects to UOS websites
+    url(r"^(?P<site>bb|udc|ms|uos)/?$", views.redirect_to_uos),
     # All website related requests link to views.layout
     # as the layout loads other dependencies as per request
     url(r"", views.layout),
--- Website/views.py
+++ Website/views.py
@@ -1,12 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.template.loader import render_to_string
 import os
+
+# Dictionary of abbreviations and their corresponding UOS websites URLs
+urls = {
+    "udc": "https://uos.sharjah.ac.ae:9050/prod_enUS/twbkwbis.P_WWWLogin",
+    "uos": "http://www.sharjah.ac.ae/en/Pages/default.aspx",
+    "ms": "https://www.outlook.com/sharjah.ac.ae",
+    "bb": "https://elearning.sharjah.ac.ae/",
+}
+
+
+# Redirects to UOS websites by abbreviation
+def redirect_to_uos(request, site):
+    return HttpResponsePermanentRedirect(urls[site])
 
 
 # Returns the front-end template "layout.html" after merging css & js includes
 def layout(request):
     return HttpResponse(
-        # Open "/static/layout.html" as a normal file as read only
+        # Open "/static/layout.html" as a normal file in read only mode
         open(os.path.dirname(__file__) + "/static/layout.html").read().replace(
             # Replace the first occurrence of "<!--INCLUDES-->" with Django rendered URLs
             "<!--INCLUDES-->", render_to_string("includes.html"), 1
