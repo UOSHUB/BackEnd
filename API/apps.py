fac427a1d6f8a007d944c6b1413367d5d1b14186
Omar Einea <eineao@gmail.com>
2018-05-06 18:39:17 +0400

    Enable Grades Checking Manually For Now (Until All Grades Are Out)

--- API/apps.py
+++ API/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
-# from .services import start_grades_checking
+from .services import start_grades_checking
 
 
 class APIConfig(AppConfig):
     name = "API"
 
     # # When app is ready
-    # def ready(self):
-    #     # Run grade checking thread
-    #     start_grades_checking()
+    def ready(self):
+        # Run grade checking thread
+        start_grades_checking()
