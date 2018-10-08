5180656ddd3c7d2434a8da2e3835a70eb831ed18
Omar Einea <eineao@gmail.com>
2018-10-08 19:02:40 +0400

    Modify Academic Calendar Fetching To Tolerate New Changes

--- Requests/homepage.py
+++ Requests/homepage.py
@@ -1,12 +1,12 @@
 from lxml.html import fromstring as __parse
 from . import seasons_codes
-import requests
+import requests, re
 
 
 # Gets this year's Academic Calendar page
 def academic_calendar():
     # HTTP get request from UOS homepage
-    return requests.get("http://www.sharjah.ac.ae/en/academics/A-Calendar/Pages/accal17-18.aspx").text
+    return requests.get("http://www.sharjah.ac.ae/en/academics/A-Calendar/Pages/accal18-19.aspx").text
 
 
 # Scrapes all terms in academic calendar
@@ -24,10 +24,10 @@ def all_terms(response):
 
 # Scrapes specified term's calendar events
 def term_events(response, term_code):
-    # Initialize events array
-    events = []
-    # Store term's year
-    year = term_code[:4]
+    # Initialize events and store year
+    year, events = term_code[:4], []
+    # Define a function to clean cell from whitespaces
+    clean = lambda cell: re.sub(" +", " ", cell.strip())
     # Format term name in "Fall Semester 2017/2017" format from term code
     term_name = f"{seasons_codes[term_code[4:]]} {year}/{int(year) + 1}"
     # Loop through available terms calendars
@@ -35,12 +35,12 @@ def term_events(response, term_code):
         # If calendar's label matches requested term name
         if term.find("label").text == term_name:
             # Loop through it's events (table rows)
-            for event in term.findall(".//tbody/tr"):
+            for event in term.findall(".//tbody/tr")[2:]:
                 # Store event row's cells
                 cells = event.findall("td")
                 # Add event's date and text to events array
                 events.append({
-                    "date": cells[2].text_content().strip(),
-                    "text": cells[4].text_content().strip()
+                    "date": clean(re.sub("(Till|-|–)", " - ", cells[1].text_content())).split(',')[0],
+                    "text": clean(cells[3].text_content())
                 })
             return events
