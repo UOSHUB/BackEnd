17a68a886e50dfd75e3eccb593b239d919e3f8d7
Omar Einea <eineao@gmail.com>
2018-12-15 22:34:14 +0400

    Merged grade_announcement & grades_summary In One Function

--- Requests/zoho/send.py
+++ Requests/zoho/send.py
@@ -21,38 +21,28 @@ def email(sender, recipient, subject, body):
     }).status_code
 
 
-# Sends a email announcement about a grade of a course
-def grade_announcement(sid, course, grade):
-    # Store grade article as "an" for (A, F) or "a" for other grades
-    article = "an" if grade == "A" or grade == "F" else "a"
-    # Send the email
-    return email(
-        # Sender is UOS HUB NO-REPLY
-        "UOS HUB <no-reply@uoshub.com>",
-        # Recipient is the student
-        __email(sid),
-        # Email subject and body details the grade and the course
-        "You got " + article + " " + grade + " in " + course,
-        "This is an automated notification from uoshub.com<br><br>" +
-        "It is to inform you that you got " + article + " <b>" + grade + "</b> in <b>" + course + "</b>"
-    )
-
-
 # Sends a email announcement about subscription and grades so far
-def grades_summary(sid, courses, gpa):
+def grades_summary(sid, courses, gpa, new_grade=None):
+    # Store grade article as "an" for (A, F) or "a" for other grades
+    if new_grade:
+        grade, course = new_grade
+        article = "an" if grade == "A" or grade == "F" else "a"
     return email(
         # Sender is UOS HUB NO-REPLY
         "UOS HUB <no-reply@uoshub.com>",
         # Recipient is the student
         __email(sid),
-        # Email subject
-        "Subscribed to UOS HUB!",
-        # Email body contains grades and GPA details
-        "<br>".join([
+        # Email subject contains new grade or first subscription message
+        f"You got {article} {grade} in {course}" if new_grade else "Subscribed to UOS HUB!",
+        # Email body contains grades, GPA details, new grade or thanks for subscription message
+        "<br>".join(([
+            f"This is to inform you that you got {article} <b>{grade}</b> in <b>{course}</b><br><br><hr>"
+            "Here's a summary of your grades including this ☝ one and your GPA:<br>"
+        ] if new_grade else [
             "Thank you for subscribing to UOS HUB Grades Checker,",
-            "You'll get another email whenever a new grade comes out.<br><br>"
-        ] + ([
-            "For now, here's a summary for what's already out:<br>",
+            "You'll get another email whenever a new grade comes out.<br><br><hr>"
+            "For now, here's a summary for what's already out:<br>"
+        ]) + ([
             "<b>Term grades so far:</b>",
             *[f"{course[1]}: {course[2]}" for course in sorted(courses, key=lambda course: len(course[1]))],
             f"<br><b>Term GPA so far</b>: {gpa['term']:.2f}",
