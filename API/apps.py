96e379c0a4d469a62447a5ef3a8d01890bd49de8
Omar Einea <eineao@gmail.com>
2018-12-18 20:01:40 +0400

    Remove Grades Checking Call From API App And Delete Services

--- API/apps.py
+++ API/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
-from .services import start_grades_checking
 
 
 class APIConfig(AppConfig):
     name = "API"
-
-    # When app is ready
-    def ready(self):
-        # Run grade checking thread
-        start_grades_checking()
